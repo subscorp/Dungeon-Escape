95e1ce90de6868612f48de4ca8486d1bc73b007c
subscorp <ori81510@gmail.com>
2023-05-25 10:43:11 +0300

    Added Phase 1 of the boss battle

---
+++ Dungeon Escape/Assets/FallRight.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallRight : StateMachineBehaviour
+{
+    private float _speed = 6f;
+    private float _jumpForce = 10f;
+    private float _prevVelocityY;
+    [SerializeField]
+    private Rigidbody2D _rigidBody;
+    private Boss _boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _rigidBody = animator.GetComponentInParent<Rigidbody2D>();
+        _boss = GameObject.Find("Boss").GetComponent<Boss>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (IsGrounded())
+        {
+            animator.SetBool("Grounded", true);
+        }
+        else
+        {
+            animator.SetBool("Grounded", false);
+            _rigidBody.velocity = new Vector2(1 * _speed, _rigidBody.velocity.y);
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 pos = _boss.transform.position;
+        //pos.y -= 5;
+        Debug.DrawRay(pos, Vector2.down * 0.25f, Color.red);
+        RaycastHit2D hitInfo = Physics2D.Raycast(pos, Vector2.down, 0.25f, 1 << 8);
+        if (hitInfo.collider != null)
+            return true;
+
+        return false;
+    }
+}
---
+++ Dungeon Escape/Assets/JumpLeft.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpLeft : StateMachineBehaviour
+{
+    private float _speed = 4f;
+    private float _jumpForce = 12f;
+    private float _prevVelocityY;
+    [SerializeField]
+    private Rigidbody2D _rigidBody;
+    private Boss _boss;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _rigidBody = animator.GetComponentInParent<Rigidbody2D>();
+        _boss = GameObject.Find("Boss").GetComponent<Boss>();
+        _rigidBody.velocity = new Vector2(-1 * _speed, _jumpForce);
+        //animator.SetBool("Grounded", false);
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_rigidBody.velocity.y < _prevVelocityY)
+        {
+            animator.SetBool("Fall", true);
+            return;
+        }
+        
+        _rigidBody.velocity = new Vector2(-1 * _speed, _rigidBody.velocity.y);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 pos = _boss.transform.position;
+        Debug.DrawRay(pos, Vector2.down * 0.25f, Color.red);
+        RaycastHit2D hitInfo = Physics2D.Raycast(pos, Vector2.down, 0.25f, 1 << 8);
+        if (hitInfo.collider != null)
+            return true;
+
+        return false;
+    }
+}
